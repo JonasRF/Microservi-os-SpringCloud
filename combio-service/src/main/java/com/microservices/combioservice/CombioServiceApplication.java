9e3412f9879d64f76127466a0c6fa700c41ea007
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2023-03-02 19:04:52 -0300

    Microserviços Spring Boot Actuator, Feign e rest

---
+++ combio-service/src/main/java/com/microservices/combioservice/CombioServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.combioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CombioServiceApplication.class, args);
+	}
+
+}
