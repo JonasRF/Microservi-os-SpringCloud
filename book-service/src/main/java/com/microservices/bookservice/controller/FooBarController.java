8bf316a172f159372bf2507dabc1e14e12484557
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2023-03-11 22:04:09 -0300

    Bulkhead

--- book-service/src/main/java/com/microservices/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/com/microservices/bookservice/controller/FooBarController.java
@@ -5,9 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 
 @RestController
 @RequestMapping("book-service")
@@ -17,15 +16,18 @@ public class FooBarController {
 
 	@GetMapping("/foo-bar")
 	//@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
-	@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+	//@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+	//@RateLimiter(name = "default")
+	@Bulkhead(name = "default")
 	public String fooBar() {
 		logger.info("Request to foo-bar is received!");
-		var response = new RestTemplate()
-				.getForEntity("http://localhost:8080/foo-bar", String.class);
-		return response.getBody();
+	//	var response = new RestTemplate()
+	//			.getForEntity("http://localhost:8080/foo-bar", String.class);
+		return "Foo-bar!";
+	//	return response.getBody();
 	}
 	
-	public String fallbackMethod(Exception e) {
-		return "fallbackMethod foo-bar!!!";
-	}
+	//public String fallbackMethod(Exception e) {
+	//	return "fallbackMethod foo-bar!!!";
+	//}
 }
